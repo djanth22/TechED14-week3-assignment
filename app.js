2845ee1b96b39f3a7e9c13362a6fd38cee6cdd9d
David Anthony <davywavy22@googlemail.com>
2024-09-13 13:02:31 +0100

    added cookie image, reset button and a count display. all are currently functional. no implementation as of yet for the upgrades or saving to local storage

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 console.log("log test");
 console.log("hello there");
+
+// cookie values
+
+let cookieCount = 1;
+let cookiesPerSecond = 0;
+const cookie = document.getElementById(`cookieImage`);
+const countContainer = document.getElementById(`cookie-count`);
+
+// function to fetch API data
+async function upgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const json = await response.json();
+}
+
+// function to update cookie count
+function updateCookieCount() {
+  let count = cookieCount++;
+  countContainer.innerHTML = ``;
+  countContainer.append(count);
+}
+
+cookie.addEventListener("click", updateCookieCount);
+
+// reset function
+const reset = document.getElementById(`reset`);
+function resetProgress() {
+  let reset = 0;
+  countContainer.innerHTML = ``;
+  countContainer.append(reset);
+  cookieCount = 1;
+}
+reset.addEventListener("click", resetProgress);
