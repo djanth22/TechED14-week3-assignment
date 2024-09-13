c2bed257ac4807a68d1d6df191fbad937ac6d62a
David Anthony <davywavy22@googlemail.com>
2024-09-13 19:37:53 +0100

    added upgrade shop with buttons along with cookies per second counter. as of yet counter is inactive so are shop buttons

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ console.log("hello there");
 
 // cookie values
 
-let cookieCount = 1;
 let cookiesPerSecond = 0;
+let cookieCount = 1 + cookiesPerSecond;
+
 const cookie = document.getElementById(`cookieImage`);
 const countContainer = document.getElementById(`cookie-count`);
-
-// function to fetch API data
-async function upgrades() {
-  const response = await fetch(
-    "https://cookie-upgrade-api.vercel.app/api/upgrades"
-  );
-  const json = await response.json();
-}
 
 // function to update cookie count
 function updateCookieCount() {
@@ -34,3 +27,55 @@ function resetProgress() {
   cookieCount = 1;
 }
 reset.addEventListener("click", resetProgress);
+
+// function to fetch API data
+async function upgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const json = await response.json();
+  shopUpgrades = json;
+  renderUpgrades();
+}
+
+let shopUpgrades = [];
+
+upgrades();
+console.log(shopUpgrades);
+
+const upgradeContainer = document.getElementById(`shop-upgrades`);
+
+function renderUpgrades() {
+  shopUpgrades.forEach(function (a) {
+    const shopNames = document.createElement("p");
+    const shopCost = document.createElement("p");
+    const increase = document.createElement("p");
+    const button = document.createElement("button");
+
+    shopNames.textContent = a.name;
+    shopNames.classList.add(`upgrade-name`);
+    shopCost.textContent = "cost " + a.cost;
+    shopCost.classList.add(`upgrade-cost`);
+    increase.textContent = "increases by " + a.increase;
+    increase.classList.add(`upgrade-increase`);
+    button.textContent = "buy";
+    button.classList.add(`buy-button`);
+
+    upgradeContainer.appendChild(shopNames);
+    upgradeContainer.appendChild(shopCost);
+    upgradeContainer.appendChild(increase);
+    upgradeContainer.appendChild(button);
+  });
+}
+
+renderUpgrades();
+
+// event listener for buttons
+
+// updating cookies per second
+
+// set interval for cookies per second
+
+if (cookiesPerSecond > 0) {
+  setInterval(updateCookieCount, 1000);
+}
